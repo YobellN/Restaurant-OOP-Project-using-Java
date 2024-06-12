470532e6b7840284583586cf693752549bf11f45
yobel <yobel@LAPTOP-NV22S3FB>
2024-06-12 23:24:03 +0700

    Commit 1st

---
+++ src/tugasbesarpbo/TugasBesarPBO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasbesarpbo;
+
+/**
+ *
+ * @author yobel
+ */
+public class TugasBesarPBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
