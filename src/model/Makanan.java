125a97eadd099a6c5a8b2127a59847b58d000581
ChenCiaLiang <aliang334chen@gmail.com>
2024-06-13 18:26:49 +0700

    Deleted Something

--- src/model/Makanan.java
+++ src/model/Makanan.java
@@ -3,16 +3,16 @@ package model;
 public class Makanan extends Menu {
     private String catatan;
 
-    public Makanan(String catatan, String nama_menu, String jenis_produk, float harga) {
-        super(nama_menu, jenis_produk, harga);
-        this.catatan = catatan;
-    } //Konstruktor tanpa id
-
-    public Makanan(String catatan, String id_menu, String nama_menu, String jenis_produk, float harga) {
-        super(id_menu, nama_menu, jenis_produk, harga);
+    public Makanan(String catatan, String id_menu, String nama_menu, String jenis_menu, float harga) {
+        super(id_menu, nama_menu, jenis_menu, harga);
         this.catatan = catatan;
     } //Konstruktor lengkap
     
+    public Makanan(String catatan, String nama_menu, String jenis_menu, float harga) {
+        super(nama_menu, jenis_menu, harga);
+        this.catatan = catatan;
+    } //Konstruktor tanpa id
+
     //getter
     public String getCatatan() {
         return catatan;
