698323ab86c1dd0bf3545222a0c5d47392536b49
seka <>
2024-06-13 18:25:49 +0700

    Mengubah Exception telepon menjadi exception khusus input angka

--- src/exception/NoTelponException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class NoTelponException extends Exception{
-    public String message(){
-        return "No Telpon Hanya Boleh Terdiri dari angka / diawali +";
-    }
-}
