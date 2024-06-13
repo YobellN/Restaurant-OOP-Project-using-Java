958abe8fd23634ef52e7c4eef670d27399ddfe7e
YobellN <yobelnugroho17@gmail.com>
2024-06-13 18:29:49 +0700

    Commit 1st

---
+++ src/dao/MakananDAO.java
@@ -0,0 +1,67 @@
+package dao;
+
+import java.sql.Statement;
+import model.Menu;
+import model.Makanan;
+import interfaceDAO.IMenuDAO;
+
+public class MakananDAO extends MenuDAO implements IMenuDAO{
+
+    public void insert(Makanan mk) {
+        super.insert(mk);
+        insertNewJenis(mk);
+    }
+
+    public void insertNewJenis(Makanan mk) {
+        con = dbCon.makeConnection();
+
+        String sql = 
+                "INSERT INTO `makanan`(`id_menu`, `catatan`) VALUES ('"
+                + mk.getId_menu()
+                + "','"
+                + mk.getCatatan()
+                + "')";
+
+        System.out.println("Adding Menu...");
+
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Added " + result + " Menu");
+            statement.close();
+        }catch (Exception e){
+            System.out.println("Error adding Menu...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();  
+    }
+
+    @Override
+    public void deleteOldJenis(String id_menu) {
+        con = dbCon.makeConnection();
+        String sql = "DELETE FROM `minuman` WHERE `id_menu` = '"+id_menu+"'";
+        System.out.println("Deleting makanan...");
+        
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Edited" + result + " Produk " + id_menu);
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Updating Produk...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+    }
+
+    public void update(Menu m, String id_menu, String catatan) {
+        Makanan mk = new Makanan(catatan, m.getId_menu(), m.getNama_menu(), m.getJenis_menu(), m.getHarga());
+        if(cekPerubahanJenis("Makanan",id_menu)){
+            deleteOldJenis(id_menu);
+            insertNewJenis(mk);
+        }else{
+            updateJenis(m, id_menu);
+        }
+        super.update(m, id_menu);
+    }
+}
---
+++ src/dao/MinumanDAO.java
@@ -0,0 +1,66 @@
+package dao;
+
+import java.sql.Statement;
+import model.Menu;
+import model.Minuman;
+import interfaceDAO.IMenuDAO;
+
+public class MinumanDAO extends MenuDAO implements IMenuDAO{
+    public void insert(Minuman mn){
+        super.insert(mn);
+        insertNewJenis(mn);
+    }
+    
+    public void insertNewJenis(Minuman mn) {
+        con = dbCon.makeConnection();
+
+        String sql = 
+                "INSERT INTO `minuman`(`id_menu`, `ukuran`) VALUES ('"
+                + mn.getId_menu()
+                + "','"
+                + mn.getUkuran()
+                + "')";
+
+        System.out.println("Adding Menu...");
+
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Added " + result + " Menu");
+            statement.close();
+        }catch (Exception e){
+            System.out.println("Error adding Menu...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();  
+    }
+    
+    @Override
+    public void deleteOldJenis(String id_menu) {
+        con = dbCon.makeConnection();
+        String sql = "DELETE FROM `makanan` WHERE `id_menu` = '"+id_menu+"'";
+        System.out.println("Deleting original...");
+        
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Edited" + result + " Menu " + id_menu);
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Updating Produk...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+    }
+    
+    public void update(Menu m, String id_menu, String ukuran) {
+        Minuman mn = new Minuman(ukuran, m.getId_menu(), m.getNama_menu(), m.getNama_menu(), m.getHarga());
+        if(cekPerubahanJenis("Minuman",id_menu)){
+            deleteOldJenis(id_menu);
+            insertNewJenis(mn);
+        }else{
+            updateJenis(m, id_menu);
+        }
+        super.update(m, id_menu);
+    }
+}
