3f9380868cd6fd0b87019c77268e8426044ac627
ChenCiaLiang <aliang334chen@gmail.com>
2024-06-15 00:37:14 +0700

    Merge origin/Progress1 into Progress1
    Conflicts:
    	src/model/Karyawan.java
    	src/model/Penitipan.java

---
+++ src/dao/ReservasiDAO.java
@@ -0,0 +1,212 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import connection.DbConnection;
+import interfaceDAO.IDAO;
+import interfaceDAO.ISearchData;
+import interfaceDAO.IShowForDropdown;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import model.Karyawan;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ReservasiDAO implements IDAO<Reservasi, String>, IShowForDropdown<Reservasi>, ISearchData<Reservasi, String>{
+    protected DbConnection dbCon = new DbConnection();
+    protected Connection con;
+    
+    @Override
+    public void insert(Karyawan C){
+        con = dbCon.makeConnection();
+        
+    String sql = 
+        "INSERT INTO `karyawan`(`id_karyawan`, `nama_karyawan`, `jabatan`, `gaji`) " +
+        "VALUES ('"+ C.getId_karyawan()+"', '"+ C.getNama_karyawan()+"', '"+ C.getJabatan()+"', '"+ C.getGaji()+"')";
+    
+        System.out.println("Adding Karyawan...");
+        
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Added " + result + " Karyawan");
+            statement.close();
+        }catch (Exception e){
+            System.out.println("Error adding Karyawan...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+    }
+    
+    
+    @Override
+    public Karyawan search(String id_karyawan){ 
+        con = dbCon.makeConnection();
+        
+        String sql = "SELECT * FROM `karyawan` WHERE id_karyawan='"+id_karyawan+"'";
+        System.out.println("Searching Karyawan...");
+        Karyawan c = null;
+        
+        try{
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if(rs != null)
+                while(rs.next())
+                    c = new Karyawan(
+                        rs.getString("id_karyawan"),
+                        rs.getString("nama_karyawan"),
+                        rs.getString("jabatan"),
+                        rs.getFloat("gaji"));
+            
+            rs.close();
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Fetching data...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+        return c;
+    }
+    
+    @Override
+    public List<Karyawan> showData(String data){
+        con = dbCon.makeConnection();
+        
+        String sql = "SELECT * FROM karyawan";
+        System.out.println("Fetching Data...");
+        List<Karyawan> c = new ArrayList();
+        
+        try{
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if(rs != null)
+                while(rs.next())
+                    c.add(new Karyawan(
+                        rs.getString("id_karyawan"),
+                        rs.getString("nama_karyawan"),
+                        rs.getString("jabatan"),
+                        rs.getFloat("gaji")));
+            
+            rs.close();
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Fetching data...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+        return c;
+    }
+    
+    @Override
+    public void update(Karyawan c, String id_karyawan){
+        con = dbCon.makeConnection();
+        
+        String sql = "UPDATE `karyawan` SET "
+                + "`id_karyawan`='" + c.getId_karyawan()+ "',"
+                + "`nama_karyawan`='" + c.getNama_karyawan()+ "',"
+                + "`jabatan`='" + c.getJabatan()+ "',"
+                + "`gaji`='" + c.getGaji()+ "' "
+                + "WHERE `id_karyawan`='" + id_karyawan + "'";
+        System.out.println("Updating karyawan");
+        
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Edited" + result + " Karyawan " + id_karyawan);
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Updating Karyawan...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+    }
+    
+    @Override
+    public void delete(String id_karyawan){
+        con = dbCon.makeConnection();
+        String sql = "DELETE FROM `karyawan` WHERE `id_karyawan` = " + id_karyawan + "";
+        System.out.println("Deleting Karyawan...");
+        
+        try{
+            Statement statement = con.createStatement();
+            int result = statement.executeUpdate(sql);
+            System.out.println("Edited" + result + " Karyawan " + id_karyawan);
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Updating Karyawan...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+    }
+    
+    @Override
+    public List<Karyawan> IShowForDropdown() {
+        con = dbCon.makeConnection();
+        
+        String sql = "SELECT * FROM `karyawan`;";
+        System.out.println("Fetching Data...");
+        List<Karyawan> data = new ArrayList();
+        
+        try{
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if(rs != null)
+                while(rs.next())
+                    data.add(new Karyawan(
+                            rs.getString("id_karyawan"),
+                            rs.getString("nama_karyawan"),
+                            rs.getString("jabatan"),
+                            rs.getFloat("gaji")
+                        )
+                    );
+            
+            rs.close();
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Fetching data...");
+            System.out.println(e);
+        }
+        
+        dbCon.closeConnection();
+        return data;
+    }
+
+    public int generateId(){
+        con = dbCon.makeConnection();
+        String sql = "SELECT MAX(CAST(SUBSTRING(id_karyawan, 2) AS SIGNED)) AS highest_number FROM karyawan WHERE id_karyawan LIKE 'K%';";
+        //mendapatkan nilai tertinggi dari id yang ada di database
+        
+        System.out.println("Generating Id...");
+        int id=0;
+        
+        try{
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            
+            if(rs != null && rs.next()){
+                if(!rs.wasNull())
+                    id = rs.getInt("highest_number")+1;
+            }
+                    
+            //memasukan id terakhir ke dalam variabel id
+                
+            rs.close();
+            statement.close();
+        }catch(Exception e){
+            System.out.println("Error Fetching data...");
+            System.out.println(e);
+        }
+        dbCon.closeConnection();
+        return id;
+    }
+}
